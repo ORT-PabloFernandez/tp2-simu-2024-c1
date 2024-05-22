74de005386e46b09a8726f787cb704266f2da0dc
rodolfoconci <r.conci@hotmail.com>
2024-05-22 19:44:46 -0300

    rodolfo conci

--- data/movies.js
+++ data/movies.js
@@ -1,8 +1,10 @@
 import getConnection from "./conn.js";
+import { ObjectId } from "mongodb";
+
 const DATABASE = "sample_mflix";
 const MOVIES = "movies";
 
-async function getAllMovies(pageSize, page) {
+export async function getAllMovies(pageSize, page) {
   const connectiondb = await getConnection();
   const movies = await connectiondb
     .db(DATABASE)
@@ -14,4 +16,26 @@ async function getAllMovies(pageSize, page) {
   return movies;
 }
 
-export { getAllMovies };
+export async function getMovieById(id) {
+  const connectiondb = await getConnection();
+  let movie;
+  try {
+    movie = await connectiondb
+      .db(DATABASE)
+      .collection(MOVIES)
+      .findOne({ _id: new ObjectId(id) })
+  } catch (error) {
+    throw new Error('Invalid ID format');
+  }
+  return movie;
+}
+
+export async function getAwardWinnerMovies(){
+  const connectiondb = await getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ "awards.wins": { $gt : 0} }, {title: 1, poster: 1, plot: 1})
+    .toArray();
+  return movies;
+}
