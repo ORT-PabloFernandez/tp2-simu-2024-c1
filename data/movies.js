bb9a40518077f4ac99a6904d305ea27bd0b05275
ngvior18 <ngvior@gmail.com>
2024-05-23 19:05:06 -0300

    Nicolas Garcia Vior -uncompleted

--- data/movies.js
+++ data/movies.js
@@ -1,8 +1,9 @@
+// import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
 const DATABASE = "sample_mflix";
 const MOVIES = "movies";
 
-async function getAllMovies(pageSize, page) {
+export async function getAllMovies(pageSize, page) {
   const connectiondb = await getConnection();
   const movies = await connectiondb
     .db(DATABASE)
@@ -14,4 +15,39 @@ async function getAllMovies(pageSize, page) {
   return movies;
 }
 
-export { getAllMovies };
+// export async function getMovie(id) {
+//   const connectiondb = await getConnection();
+//   const movie = await connectiondb
+//     .db(DATABASE)
+//     .collection(MOVIES)
+//     .findOne({ _id: new ObjectId(id) });
+
+//   return movie;
+// }
+
+export async function getAwardedMovies() {
+  //agregar paginado
+  const movies = await getAllMovies(0, 0);
+  const awardedM = movies
+    .filter((movie) => movie.awards.wins > 0)
+    .map((m) => ({
+      title: m.title,
+      poster: m.poster,
+      plot: m.plot,
+    }));
+  return awardedM;
+}
+
+export async function getMoviesByLanguage(languages, pageSize, page) {
+  const connectiondb = await getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({
+      languages: languages,
+    })
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return movies;
+}
