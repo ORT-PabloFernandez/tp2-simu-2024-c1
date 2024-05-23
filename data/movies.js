3d7c6f5d9f24e963cc1ba0d3bfebdc99a4e9cbfb
azulpavlotzky <jpazul12@hotmail.com>
2024-05-23 18:03:42 -0300

    Azul Pavlotzky

--- data/movies.js
+++ data/movies.js
@@ -2,7 +2,7 @@ import getConnection from "./conn.js";
 const DATABASE = "sample_mflix";
 const MOVIES = "movies";
 
-async function getAllMovies(pageSize, page) {
+export async function getAllMovies(pageSize, page) {
   const connectiondb = await getConnection();
   const movies = await connectiondb
     .db(DATABASE)
@@ -14,4 +14,45 @@ async function getAllMovies(pageSize, page) {
   return movies;
 }
 
-export { getAllMovies };
+
+
+export async function getWinMovie(pageSize, page) {
+  const connectiondb = await getConnection();
+  const movie = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ 'awards.wins': { $gt: 0 } })
+    .project({ plot: 1, poster: 1, title: 1 })
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+
+  return movie;
+}
+
+export async function getMoviesLanguage(pageSize, page, language) {
+  const connectiondb = await getConnection();
+  const movie = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ "languages": { $regex: new RegExp(language, "i") } })
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+    
+  return movie;
+}
+
+export async function getMoviesRanking(pageSize, page) {
+  const connectiondb = await getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ 'tomatoes.fresh': { $exists: true } })
+    .sort({ 'tomatoes.fresh': -1 }) 
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return movies;
+}
+
--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,44 @@
 import express from "express";
-import { getAllMovies } from "../data/movies.js";
+import { getAllMovies, getWinMovie, getMoviesLanguage, getMoviesRanking } from "../data/movies.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
-  const page = req.query.page ? parseInt(req.query.page) : 0;
+  try {
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 5;
+    const page = req.query.page ? parseInt(req.query.page) : 0;
+    res.json(await getAllMovies(pageSize, page));
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
 
-  res.json(await getAllMovies(pageSize, page));
+
+router.get('/win-movies', async (req, res) => {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 5;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+  res.json(await getWinMovie(pageSize, page));
+});
+
+router.get('/languages/:language', async (req, res) => {
+  try {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 5;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+  const language = req.params.language;
+  res.json(await getMoviesLanguage(pageSize, page, language));
+} catch (error) {
+  res.status(500).json({ message: 'Error al obtener las películas por idioma' });
+}
+});
+
+router.get('/ranking', async (req, res) => {
+  try {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 10;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+  res.json(await getMoviesRanking(pageSize, page));
+} catch (error) {
+  res.status(500).send({ message: error.message });
+}
 });
 
 export default router;
