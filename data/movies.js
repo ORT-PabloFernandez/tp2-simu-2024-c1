9cc264f3b0659c0bbeabbb1e490729155db5ad65
IgnacioTobiasGarciaGodoy <nachogg4@outlook.com>
2024-10-03 19:49:48 -0300

    Ignacio Garcia Godoy

--- data/movies.js
+++ data/movies.js
@@ -1,4 +1,5 @@
 import getConnection from "./conn.js";
+import { ObjectId } from "mongodb";
 const DATABASE = "sample_mflix";
 const MOVIES = "movies";
 
@@ -14,4 +15,47 @@ async function getAllMovies(pageSize, page) {
   return movies;
 }
 
-export { getAllMovies };
+async function getMovie(id) {
+  const connectiondb = await getConnection();
+  const movie = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .findOne({ _id: new ObjectId(id) });
+  return movie;
+}
+
+async function getAwardWinningMovies() {
+  const connectiondb = await getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ "awards.wins": { $gt: 0 } })
+    .project({ title: 1, poster: 1, plot: 1 })
+    .toArray();
+  return movies;
+}
+
+async function getMoviesByLanguage(language, pageSize, page) {
+  const connectiondb = await getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ languages: language })
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return movies;
+}
+
+async function getAllMoviesByFreshRating() {
+  const connectiondb = await getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ "tomatoes.fresh": { $exists: true } })
+    .sort({ "tomatoes.fresh": -1 })
+    .toArray();
+  return movies;
+}
+
+export { getAllMovies, getMovie, getAwardWinningMovies, getMoviesByLanguage, getAllMoviesByFreshRating };
--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,46 @@
 import express from "express";
-import { getAllMovies } from "../data/movies.js";
+import {
+  getAllMovies,
+  getMovie,
+  getAwardWinningMovies,
+  getMoviesByLanguage,
+  getAllMoviesByFreshRating,
+} from "../data/movies.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+  const pageSize = req.query.pageSize
+    ? parseInt(req.query.pageSize)
+    : 0;
   const page = req.query.page ? parseInt(req.query.page) : 0;
 
   res.json(await getAllMovies(pageSize, page));
+});
+
+router.get("/:id", async (req, res) => {
+  const movie = await getMovie(req.params.id);
+  res.json(movie);
+});
+
+router.get("/awards/winners", async (req, res) => {
+  const awardWinners = await getAwardWinningMovies();
+  res.json(awardWinners);
+});
+
+router.get("/language/:lang", async (req, res) => {
+  const language = req.params.lang;
+  const pageSize = req.query.pageSize
+    ? parseInt(req.query.pageSize)
+    : 0;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+  const movies = await getMoviesByLanguage(language, pageSize, page);
+  res.json(movies);
+});
+
+router.get("/ranking/fresh/all", async (req, res) => {
+  const movies = await getAllMoviesByFreshRating();
+  res.json(movies);
 });
 
 export default router;
